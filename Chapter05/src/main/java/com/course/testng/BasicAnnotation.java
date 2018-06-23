c9aaffbaa31e38bee4dbc307d74187e2490fadca
辜祥宏 <guxianghong@gugu.local>
2018-06-23 10:25:45 +0800

    基本注解，了解注解的执行顺序

---
+++ Chapter05/src/main/java/com/course/testng/BasicAnnotation.java
@@ -0,0 +1,56 @@
+package com.course.testng;
+
+import org.testng.annotations.*;
+
+/*
+记住执行顺序 BeforeSuite--BeforeClass--BeforeMethod--Test--AfterMethod--AfterClass
+--AfterSuite
+ */
+
+public class BasicAnnotation {
+
+    //最基本的注解，用来把方法标记为测试的一部分
+    @Test
+    public void testCase1(){
+        System.out.println("这是测试用例1");
+    }
+
+    @Test
+    public void testCase2(){
+        System.out.println("这是测试用例2");
+    }
+
+    //在每一个方法之前运行
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("before method,这是在每一个测试方法之前运行的");
+    }
+
+    //在每一个方法之后运行
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("after method，这是在每一个测试方法之后运行的");
+    }
+
+    //在类运行之前运行
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("before class,这是在类运行之前运行的方法");
+    }
+
+    //在类运行之前运行
+    @AfterClass
+    public void afterClass(){
+        System.out.println("after class,这是在类运行之后运行的方法");
+    }
+
+    @BeforeSuite
+    public void beforeSuite(){
+        System.out.println("before suite,测试套件");
+    }
+
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("after suite,测试套件");
+    }
+}
